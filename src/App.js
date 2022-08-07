df93b422710c1951de664c4082c43a3f4c7c88ce
Shahnwaz Khan <shahnwazkhan2007@gmail.com>
2022-08-07 12:04:59 +0530

    first commit

---
+++ src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+import Simulation from './Simulation/Simulation';
+function App() {
+  return (
+    <div className="App">
+     <Simulation/>
+    </div>
+  );
+}
+
+export default App;
